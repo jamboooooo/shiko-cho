9fd7c7f48905c707c9f2f461c0c1197ded642b17
jamboooooo <hisllam35@gmail.com>
2021-06-11 16:45:49 +0300

    begin working

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express")
 const app = express()
 const hbs = require('express-handlebars')
+const path = require('path')
 
 const cors = require('cors')
 
 app.set("view engine", "handlebars");
 app.engine("handlebars", hbs());
 app.use(cors())
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000);
